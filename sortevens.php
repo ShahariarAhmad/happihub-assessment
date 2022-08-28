0eb9b9db526afc3377e5351c26b70dc00162a097
Shahariar Ahmad <71463303+ShahariarAhmad@users.noreply.github.com>
2022-08-29 01:22:30 +0600

    Add files via upload

---
+++ sortevens.php
@@ -0,0 +1,36 @@
+<?php
+echo "<pre>";
+$num = [1, 10, 13, 2, 4, 5, 8, 7, 11];
+
+
+
+function findEven($arr)
+{
+
+    $flag = 0;
+  
+    for ($i = 0; $i < count($arr); $i++) {
+
+
+        if ($arr[$i] % 2 !== 0) {
+
+            echo " <br> Odd number " . $arr[$i] . ' removed <br><br><br><br>';
+            unset($arr[$i]);
+            $newarr = array_reverse($arr);
+            print_r($refreshed = array_values($newarr));
+            echo  ' **************************<br><br>';
+
+            foreach ($arr as $v) {
+                if ($v % 2 !== 0)
+                    $flag += 1;
+            }
+            if ($flag !== 0) {
+
+                return findEven($refreshed);
+            }
+        }
+    }
+}
+
+
+findEven($num);
